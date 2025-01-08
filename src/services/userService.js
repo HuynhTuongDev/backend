d87e4a580065034291d944e275d601ddb9e10fe4
Finn <lehuynhtuong13@gmail.com>
2025-01-08 08:27:21 +0700

    images data

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,18 +3,28 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const createUserService = async (fullName, email, password) => {
     try {
-        //hash User Passwword 
-        const hashPasswword = await bcrypt.hash(password, saltRounds)
-        //save to database
-        let result = await User.create({
+        // Kiểm tra email đã tồn tại chưa
+        const existingUser = await User.findOne({ where: { Email: email } });
+        if (existingUser) {
+            console.log("Email already exists");
+            return null; // Hoặc trả về một thông báo lỗi
+        }
+
+        // Hash mật khẩu
+        const hashPassword = await bcrypt.hash(password, saltRounds);
+
+        // Tạo người dùng mới
+        const newUser = await User.create({
             FullName: fullName,
             Email: email,
-            Password: hashPasswword,
+            Password: hashPassword,
             Role: 'Admin'
-        })
-        return result;
+        });
+
+        // Trả về người dùng mới tạo
+        return newUser;
     } catch (error) {
-        console.log(error);
+        console.log("Error creating user:", error);
         return null;
     }
 }
